c8636c6cf475b651f2f767273316e66084c0e2c6
Delmac <delalitengue.dev@gmail.com>
2023-09-13 17:35:28 +0000

    done with the site but must complete Nav component

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import Button from '../components/Button'
+import { offer } from '../assets/images'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img src={offer} alt="offer" width={776} height={687} className='object-contain w-full'/>
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl font-bold lg:max-w-lg'>
+          <span className='text-coral-red'>Special </span>
+          Offers
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Emback on a shopping journey that redefines your experience with unbeatable deals.
+          From premier selections to incredible savings, we offer unparalled value that sets us apart.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Navigate a realm of possibilities designed to fulfill your
+          unique desires, surpassing the loftiest expectations. Your journey with us
+          is nothing short of exceptional.
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label = "View Details" iconURL = {arrowRight} />
+          <Button label="Learn more" backgroundColor = "bg-white" borderColor = "border-slate-gray" textColor="text-slate-gray" />
+        </div>
+      </div>
+    </section>
   )
 }
 
